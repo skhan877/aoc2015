56e8edac1d1f893076a9c9e2147a2d82966a6f63
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2024-11-17 21:13:25 +0000

    updates

--- day2.py
+++ day2.py
@@ -3,21 +3,29 @@ def parse_input(input):
     arr = []
     with open(input, "r") as f: 
         for line in f:
-            arr.append(line)
+            arr.append(line.replace("\n",""))
 
     return arr 
 
 
-def part_one():
-    pass 
+def part_one(input):
+    
+    for dims in input:
+        l, w, h = dims.replace("x", " ").split(" ")
+        print(dims, l, w, h)
 
 
-def part_two():
+def part_two(input):
     pass 
 
 
 def main():
-    pass 
+    f = "inputs//day2.txt"
+    data = parse_input(f)
+    # print(data[:10])
+
+    print(part_one(data[:10]))
+    # print(part_two(data))
 
 
 if __name__ == "__main__":
