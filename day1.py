1c6095d9ed95a23102c518ba1f75ab98e52d012a
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2024-11-17 10:26:12 +0000

    day 1 complete

--- day1.py
+++ day1.py
@@ -8,10 +8,43 @@ def parse_input(input):
 
     return arr 
 
+def part_one(input):
+
+    level = 0 
+
+    for par in input[0]: 
+        # print(par)
+        if par == "(":
+            level += 1
+        elif par == ")":
+            level -=1 
+
+    return level 
+
+
+def part_two(input): 
+
+    instructions = input[0]
+    n = len(instructions)
+    level = 0 
+
+    for i in range(n): 
+        if instructions[i] == "(":
+            level += 1
+        elif instructions[i] == ")":
+            level -=1 
+        
+        if level == -1:
+            return i  + 1
+
+    return -1
+
 
 def main(): 
     f = "inputs//day1.txt"
-    print(parse_input(f))
+    data = parse_input(f)
+    # print(part_one(data))
+    print(part_two(data))
 
 
 if __name__ == "__main__": 
