0dd83e399ba4aa399d9cdaea85c6ab8b70fcd60b
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2024-11-17 21:02:51 +0000

    day 2 initial commit

---
+++ day2.py
@@ -0,0 +1,24 @@
+def parse_input(input): 
+    
+    arr = []
+    with open(input, "r") as f: 
+        for line in f:
+            arr.append(line)
+
+    return arr 
+
+
+def part_one():
+    pass 
+
+
+def part_two():
+    pass 
+
+
+def main():
+    pass 
+
+
+if __name__ == "__main__":
+    main()
