e9d41cb47df37301e4b7b2c985fb3c71c9ae4ca7
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2024-11-18 22:32:01 +0000

    day 4 complete

--- day4.py
+++ day4.py
@@ -10,19 +10,28 @@ def parse_input(input):
 
 def part_one(input):
 
-    pass
+    import hashlib 
+
+    for i in range(1000000000):
+        hsh = hashlib.md5((input+str(i)).encode("utf-8")).hexdigest()
+        if hsh[:5] == "00000":
+            return i, hsh 
 
 
 def part_two(input):
     
-    pass
+    import hashlib 
+
+    for i in range(1000000000):
+        hsh = hashlib.md5((input+str(i)).encode("utf-8")).hexdigest()
+        if hsh[:6] == "000000":
+            return i, hsh 
 
 
 def main():
     data = "ckczppom"
-    
-    print(part_one(data))
-    # print(part_two(data))
+    # print(part_one(data))
+    print(part_two(data))
 
 
 if __name__ == "__main__":
