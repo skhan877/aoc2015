9f21a68cbbd727c4221ca3fbff8049d752e9242e
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2024-11-19 15:44:02 +0000

    day 5 updates

--- day5.py
+++ day5.py
@@ -10,7 +10,34 @@ def parse_input(input):
 
 def part_one(input):
 
-    pass
+    vowels = ['a','e','i','o','u']
+    disallowed = ['ab', 'cd', 'pq', 'xy']
+    good_strings = []
+
+    for strng in input: 
+
+        vowel_count = 0
+        dupe_count = 0
+        disallowed_count = 0
+        
+        if strng[0] in vowels:
+            vowel_count += 1
+        
+        for i in range(1, len(strng)):
+            # rule 1
+            if strng[i] in vowels:
+                vowel_count += 1
+            # rule 2
+            if strng[i] == strng[i-1]:
+                dupe_count += 1
+            # rule 3
+            if strng[i-1:i+1] in disallowed:
+                disallowed_count += 1
+        
+        if vowel_count >= 3 and dupe_count >= 1 and disallowed_count == 0:
+            good_strings.append(strng)
+
+    return len(good_strings)
 
 
 def part_two(input):
@@ -21,8 +48,11 @@ def part_two(input):
 def main():
     f = "inputs//day5.txt"
     data = parse_input(f)
+    # print(data)
 
-    # print(part_one(data))
+    # sample = ['ugknbfddgicrmopn', 'jchzalrnumimnmhp', 'haegwjzuvuyypxyu', 'dvszwmarrgswjxmb']
+
+    print(part_one(data))
     # print(part_two(data))
 
 
