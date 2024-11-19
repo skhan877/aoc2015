175036b0e149f1bdf2eeb4d91cd772529bd7f96e
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2024-11-19 22:32:07 +0000

    day 5 initial commit

--- day5.py
+++ day5.py
@@ -41,66 +41,91 @@ def part_one(input):
 
 
 def part_two(input):
-    
-    from collections import Counter
 
-    nice_strings = []
+    def two_pairs(s):
+        for i in range(2, len(s)-1):
+            a = s[i-2: i]
+            b = s[i: ]
+            if a == b:
+                return True 
+        return False
+    
+    def valid_trios(s):
+        for i in range(len(s)-2):
+            if s[i] == s[i+2]:
+                return True 
+        return False
+    
+    nice_strings = 0 
 
     for strng in input:
-        # print(strng)
-        n = len(strng)
-        if n < 3:
-            continue
+        if two_pairs(strng) and valid_trios(strng):
+            nice_strings += 1
 
-        pairs = [strng[0]+strng[1]]
-
-        i, j = 1, 2
-        while j < n:
-            # rule 1
-            if strng[i]+strng[j] != pairs[-1]:
-                pairs.append(strng[i]+strng[j])
-                i += 1
-                j += 1
-            else:
-                i += 2
-                j += 2
-        
-        pairs_count = 0
-        cnter = Counter(pairs)
-        for k, v in cnter.items():
-            if v >= 2:
-                pairs_count += 1
-        
-        trio = 0
-
-        p, q, r = 0, 1, 2
-        while r < n:
-            # rule 2 
-            if strng[p] == strng[r]:
-                # print("valid trio", strng[p], strng[q], strng[r])
-                trio += 1
-            p += 1
-            q += 1
-            r += 1
-
-        if pairs_count >= 1 and trio >= 1:
-            nice_strings.append(strng)
-        
-        # print('')
+    return nice_strings
     
-    return len(nice_strings)
+    # from collections import Counter
+
+    # nice_strings = []
+
+    # for strng in input:
+    #     # print(strng)
+    #     n = len(strng)
+    #     if n < 3:
+    #         continue
+
+    #     pairs = [strng[0]+strng[1]]
+
+    #     i, j = 1, 2
+    #     while j < n:
+    #         # rule 1
+    #         if strng[i]+strng[j] != pairs[-1]:
+    #             pairs.append(strng[i]+strng[j])
+    #             i += 1
+    #             j += 1
+    #         else:
+    #             i += 2
+    #             j += 2
+        
+    #     pairs_count = 0
+    #     cnter = Counter(pairs)
+    #     for k, v in cnter.items():
+    #         if v >= 2:
+    #             pairs_count += 1
+    #             # print(k)
+        
+    #     trio = 0
+
+    #     p, r = 0, 2
+    #     while r < n:
+    #         # rule 2 
+    #         if strng[p] == strng[r]:
+    #             # print("valid trio", strng[p], strng[r-1], strng[r])
+    #             trio += 1
+    #         p += 1
+    #         r += 1
+
+    #     if pairs_count >= 1 and trio >= 1:
+    #         nice_strings.append(strng)
+    #         # print("is nice string", strng, len(nice_strings))
+        
+    #     # print('')
+    
+    # return len(nice_strings), nice_strings
 
 
 def main():
     f = "inputs//day5.txt"
     data = parse_input(f)
     # print(data)
+    # data = data[-20:]
 
-    # sample1 = ['qjhvhtzxzqqjkmpb', 'jchzalrnumimnmhp', 'haegwjzuvuyypxyu', 'dvszwmarrgswjxmb']
-    sample2 = ['uurcxstgmygtbstg', 'qjhvhtzxzqqjkmpb', 'xxyxx', 'ieodomkazucvgmuy', 'aaa']
+    # sample = ['qjhvhtzxzqqjkmpb', 'jchzalrnumimnmhp', 'haegwjzuvuyypxyu', 'dvszwmarrgswjxmb']
+    sample = ['uurcxstgmygtbstg', 'qjhvhtzxzqqjkmpb', 'xxyxx', 'ieodomkazucvgmuy', 'aaa']
+    # sample = ['xckozymymezzarpy', 'cxoaaphylmlyljjz', 'ionndmdwpofvjnnq', 'ywehopujowckggkg', 'cekjbablkjehixtj', 'ycsfscegcexcnbwq', 'qpjdsocrtwzpjdkd', 'jdckfjdtjsszdzhj', 'yohdmucunrgemqcu', 'xvgpghhdtpgvefnk', 'iiewevdzbortcwwe', 'twichytatzoggchg', 'qmgvroqwrjgcycyv', 'ezvjijcjcyszwdjy', 'dodjadoqyxsuazxt', 'jswabfbzpnhhdbpn', 'yjdjeckmsrckaagx', 'avdtdppodpntojgf', 'hoyqdzofddedevsb', 'bubeonwbukprpvhy', 'romyflhrarxchmyo', 'jfhobjxionolnouc', 'lfgehftptlfyvvaj', 'pkldomvvklefcicw', 'arerrohyhhkmwhyo', 'pvcvcsupfwsmeacs', 'gqgqyjvqmfiwiyio', 'eygyuffeyrbbhlit', 'jrcrdjrvsyxzidsk', 'njgolfwndqnwdqde', 'rurtxgibkeaibofs', 'sfrwfazygygzirwd', 'yccaifzmvbvwxlcc', 'szephmeegvegugdt', 'dtuhjnlaliodtlvh', 'vjqkhkhjlmvpwkud', 'hsvikeyewfhsdbmy', 'aapqpwapzyjnusnr', 'xckvsnkvnvupmirv', 'oyzhmirzrnoytaty', 'rpepcfpelvhzzxzj', 'lmfiylebtzwtztmx', 'uyibwcitxixjfwcr', 'yktkmkokmfslgkml', 'icdawnmfnpnmyzof', 'fgysnxrnfnxprdmf', 'mqzxvvskslbxvyjt', 'qryjbohkprfazczc', 'hubpbvxknepammep', 'gthxhaapfpgtilal']
 
     # print(part_one(data))
-    print(part_two(sample2))
+    print(part_two(sample))
 
 
 if __name__ == "__main__":
