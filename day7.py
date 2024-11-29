300501df84b41582874629d5fb056e169a2a8e27
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2024-11-29 21:21:37 +0000

    updates

--- day7.py
+++ day7.py
@@ -10,7 +10,19 @@ def parse_input(input):
 
 def part_one(input):
 
-    pass 
+    signals = {} 
+
+    for instr in input: 
+        print(instr)
+        instr = instr.split(" -> ")
+        print(instr)
+        print(instr[0].split(" "))
+        print("")
+
+    print(123 & 456)
+    # print(6 >> 1)
+
+    return signals 
 
 
 def part_two(input, size=1000):
@@ -18,13 +30,24 @@ def part_two(input, size=1000):
     pass 
 
 
+# def convert_syntax(word): 
+
+#     operators = {"AND": &,
+#         "OR": |,
+#         "XOR": ^,
+#         "NOT": ~,
+#         "RSHIFT": >>,
+#         "LSHIFT": <<
+#         }
     
+#     return operators[word]
+
 
 def main():
     f = "inputs//day7.txt"
     data = parse_input(f)
     # data = data[:4]
-    print(data[:4])
+    # print(data[:4])
 
     sample = ['123 -> x',
             '456 -> y',
@@ -36,7 +59,7 @@ def main():
             'NOT y -> i'
             ]
 
-    # print(part_one(data))
+    print(part_one(sample))
     # print(part_two(data))
 
 
